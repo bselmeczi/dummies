18525d35dc4c8334bdf0298b7ab45a12312ef146
Balazs <bselmeczi@yahoo.com>
2023-11-01 22:41:51 +0100

    moving greeting message to the property file. using ENV VAR for the message.

--- src/main/java/com/balazs/dummies/demo/DummiesController.java
+++ src/main/java/com/balazs/dummies/demo/DummiesController.java
@@ -1,12 +1,18 @@
 package com.balazs.dummies.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DummiesController {
+
+    @Value("${greeting.message.from.env}")
+    private String greetingMessage;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from Docker! Istvan loves shiny things!!!";
+        return greetingMessage;
     }
+
 }
